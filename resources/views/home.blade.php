f3a2c09f2480a84f45fbec9de1e645dc11ff61e5
22091397054_Salsabilla Athifah Yonara <124547235+sathipyn@users.noreply.github.com>
2024-03-19 17:36:19 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('title','Home')
 
 @section('content')
-    <h3>TUGAS PEMROGRAMAN PERENGKAT LUNAK </h3>
+    <h3>TUGAS PEMROGRAMAN PERANGKAT LUNAK </h3>
     <p>KELOMPOK 8 </p>
      <p>   1. Salsabilla Athifah Yonara - 22091397054 </p>
      <p>   2. Ilma Nur Hidayah - 22091397064 </p>
